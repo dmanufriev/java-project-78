1566c581d5b588c70285ca26fd8120bd10e0df97
Dmitry Anufriev <dm.anufriev@gmail.com>
2024-03-12 13:32:14 +0300

    Step 2. String validator added

---
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -0,0 +1,39 @@
+package hexlet.code.schemas;
+
+public class StringSchema {
+    private boolean required;
+    private int minLength;
+    private String substring;
+
+    public StringSchema required() {
+        required = true;
+        return this;
+    }
+
+    public StringSchema minLength(int length) {
+        minLength = length;
+        return this;
+    }
+
+    public StringSchema contains(String substr) {
+        this.substring = substr;
+        return this;
+    }
+
+    public boolean isValid(String string) {
+
+        if ((null == string) || (string.isEmpty())) {
+            return !required;
+        }
+
+        if (string.length() < minLength) {
+            return false;
+        }
+
+        if ((null != substring) && !string.contains(substring)) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ app/src/test/java/ValidatorTest.java
@@ -0,0 +1,43 @@
+import hexlet.code.Validator;
+import hexlet.code.schemas.StringSchema;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidatorTest {
+    private Validator v;
+    private StringSchema schema;
+
+    @BeforeEach
+    void beforeEach() {
+        v = new Validator();
+        schema = v.string();
+    }
+
+    @Test
+    void requiredTest() {
+        assertThat(schema.isValid("")).isEqualTo(true);
+        assertThat(schema.isValid(null)).isEqualTo(true);
+        schema.required();
+        assertThat(schema.isValid(null)).isEqualTo(false);
+        assertThat(schema.isValid("")).isEqualTo(false);
+    }
+
+    @Test
+    void minLengthTest() {
+        String testStr = "hexlet";
+        assertThat(schema.isValid(testStr)).isEqualTo(true);
+        assertThat(schema.minLength(15).isValid(testStr)).isEqualTo(false);
+    }
+
+    @Test
+    void containsTest() {
+        String testStr = "what does the fox say";
+        assertThat(schema.contains("wh").isValid(testStr)).isEqualTo(true);
+        assertThat(schema.contains("what").isValid(testStr)).isEqualTo(true);
+        assertThat(schema.contains("whatthe").isValid(testStr)).isEqualTo(false);
+        assertThat(schema.isValid(testStr)).isEqualTo(false);
+        assertThat(schema.contains("what").isValid(testStr)).isEqualTo(true);
+    }
+}
